3167078717b5cae4f9a71b3f9dd8301b85c1dd84
MuhYafiTaqq <twizz1366@gmail.com>
2025-04-29 17:44:36 +0700

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FaPlus } from "react-icons/fa6";
 
 export default function Navbar() {
   return (
-    <nav className='fixed bottom-0 w-90 w-full flex justify-between bg-neutral-950 border-t-1 border-white/5 py-2 px-3'>
+    <nav className='fixed bottom-0 w-full flex justify-between bg-neutral-950 border-t-1 border-white/5 py-2 px-3'>
         <div className='h-15 w-18 flex flex-col gap-1.5 justify-center items-center'>
             <LuLayoutTemplate className='text-white/35 text-2xl' />
             <Link to="/" className='text-white/35 text-xs'>Template</Link>
